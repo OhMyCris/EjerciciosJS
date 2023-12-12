c677f6c44ea0dfb8fd21b04c35718a72a1fc3440
OhMyCris <cristina.crespo@bootcamp-upgrade.com>
2023-12-12 09:22:44 +0100

    extra a medias

--- S1/extra/exercise-1.js
+++ S1/extra/exercise-1.js
@@ -1,1 +1,8 @@
 const users = [{name: "Abel", years: 43}, {name: "Maria", years: 18}, {name: "Pedro", years: 14}, {name: "Samantha", years: 32}, {name: "Raquel", years: 16}];
+
+for (let i = 0; i <= users.length; i++) {
+    if (users.years < 18) {
+        console.log("hola " + users.length);
+    }
+    console.log(users.name);
+}
---
+++ S1/extra/exercise-2.js
@@ -0,0 +1,6 @@
+const fruits = ['Strawberry', 'Banana', 'Orange', 'Apple'];
+const foodSchedule = [{name: "Salad", isVegan: true},{name: "Salmon", isVegan: false}, {name: "Tofu", isVegan: true}, {name: "Burger", isVegan: false}, {name: "Rice", isVegan: true}, {name: "Pasta", isVegan: true}];
+
+for (var = i, i < foodSchedule.length, i++) {
+    
+}
